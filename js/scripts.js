32294c2bdf86ca8fe7e4a222f10e55cf7977669a
Jaddisonapps <james@addisonapplications.com>
2021-04-07 13:14:07 -0700

    add responsive nav bar and minimize logo

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
-document.querySelector('button').addEventListener('click',function(){
-	console.log("Hello")
-})
+// document.querySelector('button').addEventListener('click',function(){
+// 	console.log("Hello")
+// })
+
+
+
+
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+
+
+
+	burger.addEventListener('click', () => {
+		//Toggle Nav
+		nav.classList.toggle('nav-active');
+
+		//animate links
+		navLinks.forEach((link, index) => {
+			if(link.style.animation) {
+				link.style.animation = '';
+			} else {
+				link.style.animation = `navLinkFade 1s ease forwards ${index / 7 + 0.3}s`;
+			}
+		});
+		//Burger Animation
+		burger.classList.toggle('toggle');
+	}); 
+
+}
+
+navSlide();
